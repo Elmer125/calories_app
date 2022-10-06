b4f4986fb6a9bd7537cbb74a9a118765f64ae3f8
Elmer125 <elmermauricio05@hotmail.com>
2022-10-06 14:24:06 -0500

    Adds devise and fixed navbar

--- Gemfile
+++ Gemfile
@@ -73,3 +73,5 @@ end
 
 
 gem "kaminari", "~> 1.2"
+
+gem "devise", "~> 4.8"
--- app/models/calorie.rb
+++ app/models/calorie.rb
@@ -1,6 +1,6 @@
 class Calorie < ApplicationRecord
   paginates_per 12
-
+  belongs_to :user
   def is_burned_or_consumed?
     if is_burned
       'Burned'
